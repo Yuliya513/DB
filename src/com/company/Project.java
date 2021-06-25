e52ff355b15ea584472efcc718b80da2680ff39d
Yuliya513 <83278354+Yuliya513@users.noreply.github.com>
2021-06-25 19:25:19 +0300

    Multiplication table

---
+++ src/com/company/Project.java
@@ -0,0 +1,14 @@
+package JavaDB_001;
+
+public class Project {
+
+    public static void main(String[] args){
+        for(int i = 0;i < 1000;i++){
+            for(int j = 0; j <= 1000; j++){
+                System.out.println(i +" X "+j+" = "+i*j);
+            }
+            System.out.println("-----------------");
+        }
+    }
+
+}
